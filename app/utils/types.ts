2b29e507824786c9dea28bd5378d451dd0a5ffc7
Kenneth Jones <alexanderjonesmadrid@gmail.com>
2026-08-05 11:20:14 -0400

    ELECTRIC3-17: define Animal type schema

---
+++ app/utils/types.ts
@@ -0,0 +1,27 @@
+export type ConservationStatus = 
+| 'Least Concern'
+| 'Near Threatened'
+| 'Vulnerable'
+| 'Endangered'
+| 'Critically Endangered';
+
+export type TaxonomicGroup =
+| 'fish'
+| 'reptile'
+| 'amphibian'
+| 'mammal'
+| 'bird';
+
+export interface Animal {
+    id: string;
+    commonName: string;
+    scientificName?: string;
+    photoUrl: string;
+    taxonomicGroup: TaxonomicGroup;
+    nativeRegion: string;
+    habitatDescription: string;
+    conservationStatus: ConservationStatus;
+    conservationStatusIcon: string;
+    threats: string[];
+    funFact: string;
+}
